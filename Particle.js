fea95b9310c0de085a37dc3c0bc3a345e642b2c8
xichomea <xichomea@gmail.com>
2025-12-21 01:02:18 +0900

    Particle에 mass 개념 추가

--- Particle.js
+++ Particle.js
@@ -3,10 +3,12 @@ class Particle {
         this.pos = createVector(random(width), random(height));
         this.vel = p5.Vector.random2D();
         this.acc = createVector(0 ,0);
+        this.mass = random(0.5, 1.5);
     }
 
 applyForce(force) {
-    this.acc.add(force);
+    let f = p5.Vector.div(force, this.mass);
+    this.acc.add(f);
 }
 
 update() {
