47b9de2d443c2239451df8923d506c6d2ef349f7
xichomea <xichomea@gmail.com>
2025-12-21 00:34:56 +0900

    파티클 시스템에서 파티클 생성 오타 수정

--- ParticleSystem.js
+++ ParticleSystem.js
@@ -2,7 +2,7 @@ class ParticleSystem {
     constructor() {
         this.particles = [];
         for (let i = 0; i < 50; i++) {
-        this.particles.push(new particle());
+        this.particles.push(new Particle());
     }
     }
 } 
